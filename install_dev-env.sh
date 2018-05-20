0cde1ec54b690e901d45b6399ccaaf5a0e3289fa
mundusnine <bastiennadeau@gmail.com>
2018-05-20 16:03:27 -0400

    Edited the install dev-env to make sure we didnt install the zip before installing gitextensions

--- install_dev-env.sh
+++ install_dev-env.sh
@@ -1,27 +1,12 @@
+BASEDIR=$(dirname "$0")'/install_gitextensions.sh'
+echo $BASEDIR
 sudo apt-get -y install git 
 sudo apt-get -y install kdiff3
 sudo apt-get -y install mono-complete
 sudo apt-get -y install ssh-askpass-fullscreen
-cd 
+cd
 cd Downloads
-wget https://github.com/gitextensions/gitextensions/releases/download/v2.51.01/GitExtensions-2.51.01-Mono.zip
-unzip GitExtensions-2.51.01-Mono.zip
-sudo mkdir /opt/GitExtensions/
-sudo mv GitExtensions /opt/GitExtensions/
-sudo chmod +x /opt/GitExtensions/GitExtensions/GitExtensions.exe
-cd ~/.local/share/applications/
-sudo echo '[Desktop Entry]
-Encoding=UTF-8
-Version=1.0
-Name=Gitextensions
-Comment=git-tool
-Exec=/opt/GitExtensions/GitExtensions/GitExtensions.exe
-Icon=git-extensions-logo-final-256.ico
-Terminal=false
-Type=Application
-Categories=Development; versionning;
-Name[en_US]=gitextensions.desktop
-GenericName[en_US]=Version tool 
-Comment[en_US]=Create repos/push/pull'>> gitextensions.desktop
+[-f GitExtensions-2.51.01-Mono.zip ] && /bin/bash/ $BASEDIR || echo 'Gitextensions already installed'
 sudo snap install vscode --classic
 sudo snap install godot --classic
+sudo apt-get -y install llvm
